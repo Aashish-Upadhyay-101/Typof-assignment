03e68bc28e8f4da0ddda526041108a77b2f2ad01
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-05-16 20:26:13 +0545

    dotenv setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import express, { Request, Response, Express } from "express";
 
+require("dotenv").config();
+
 const app: Express = express();
+
+const port = process.env.PORT || 8080;
 
 app.get("/", (req: Request, res: Response) => {
     console.log("helloworld");
     res.status(200).json({
         status: "success",
         message: "Hello Typof",
-    })
-})
+    });
+});
 
-app.listen(8080, () => {
-    console.log(`Server running on http://localhost:8080`);
-})
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
